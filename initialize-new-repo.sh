0464b02fcc26aaff8536aa0c89335749839847be
Amlan Alok <amlanalok@gmail.com>
2022-03-05 21:44:02 -0600

    commands to initialize new repo

---
+++ initialize-new-repo.sh
@@ -0,0 +1,9 @@
+
+
+git init
+git add .
+git commit -m "first commit"
+git remote add origin <repo link>
+# Ex - git remote add origin git@github.com:AmlanAlok/git-commands.git
+git push origin <branch-name>
+# Ex - git push origin master
